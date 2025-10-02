b744a51987309a05acdf0e27837cd8984afc59bd
Lennart <lennart.3108@icloud.com>
2025-10-02 22:48:46 +0200

    build: update API base resolution and improve logging in backend

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -4,3 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+// Für Tests immer Same-Origin benutzen
+jest.mock('./config', () => ({ API_BASE: '/api' }));
+
